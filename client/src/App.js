09507c93cc9240fa32eb7938b10cfaf85a4e6864
Pablo Magro <pablom@traffic.net.nz>
2020-12-24 01:42:13 +1300

    Updated header v2

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib Calculator version KUBERNETES!
+            Fib Calculator version KUBERNETES!YES.===.SI! :)(:
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
